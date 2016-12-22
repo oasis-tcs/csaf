b9e3442183c42185428a776338fb2b69fe94580e
Omar Santos <santosomar@gmail.com>
2016-12-21 19:53:25 -0500

    Draft CSAF Documentation Website

---
+++ csaf.github.io/css/heroic-features.css
@@ -0,0 +1,21 @@
+/*
+ * Start Bootstrap - Heroic Features (http://startbootstrap.com/)
+ * Copyright 2013-2016 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
+
+body {
+    padding-top: 70px; /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+}
+
+.hero-spacer {
+    margin-top: 50px;
+}
+
+.hero-feature {
+    margin-bottom: 30px;
+}
+
+footer {
+    margin: 50px 0;
+}
