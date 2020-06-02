7b33b74f8ea817597122895514ff276a07fc0473
Tobias Limmer <tobias.limmer@siemens.com>
2020-06-02 17:10:46 +0200

    Generate strict schema, perform tests against it

--- csaf_2.0/test/run_tests.sh
+++ csaf_2.0/test/run_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SCHEMA=csaf_2.0/json_schema/csaf_json_schema.json 
+ORIG_SCHEMA=csaf_2.0/json_schema/csaf_json_schema.json 
+STRICT_SCHEMA=strict_schema.json
 VALIDATOR=csaf_2.0/test/validator.py
+STRICT_GENERATOR=csaf_2.0/test/generate_strict_schema.py
 
 FAIL=0
 
@@ -19,6 +21,15 @@ validate() {
 
 }
 
+SCHEMA=$ORIG_SCHEMA
+validate csaf_2.0/examples/CVE-2018-0171-modified.json
+validate csaf_2.0/examples/cvrf-rhba-2018-0489-modified.json
+ 
+echo -n "Generating strict schema ... "
+python3 $STRICT_GENERATOR $ORIG_SCHEMA > $STRICT_SCHEMA
+echo done
+
+SCHEMA=$STRICT_SCHEMA
 validate csaf_2.0/examples/CVE-2018-0171-modified.json
 validate csaf_2.0/examples/cvrf-rhba-2018-0489-modified.json
 
