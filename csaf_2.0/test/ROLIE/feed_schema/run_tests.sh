2df1632672001b388925aea606608ec173845a60
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2022-03-17 18:25:20 +0100

    JSON schema for ROLIE feed
    - addresses parts of oasis-tcs/csaf#480
    - add test for ROLIE feed schema
    - integrate into GitHub actions

---
+++ csaf_2.0/test/ROLIE/feed_schema/run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ORIG_SCHEMA=csaf_2.0/json_schema/ROLIE_feed_json_schema.json 
+STRICT_SCHEMA=ROLIE_feed_strict_schema.json
+VALIDATOR=csaf_2.0/test/validator.py
+STRICT_GENERATOR=csaf_2.0/test/generate_strict_schema.py
+TESTPATH=csaf_2.0/examples/ROLIE/*feed*.json
+
+FAIL=0
+
+# go to root of git repository
+cd `dirname $0`/../../../..
+
+validate() {
+  printf "%s" "Testing file $1 against schema ${SCHEMA} ... "
+  if python3 ${VALIDATOR} ${SCHEMA} $1; then
+    printf "%s\n" SUCCESS
+  else
+    printf "%s\n" FAILED
+    FAIL=1
+  fi
+
+}
+
+test_all() {
+  for i in ${TESTPATH}
+  do
+    validate $i
+  done
+} 
+
+SCHEMA=${ORIG_SCHEMA}
+test_all
+
+ 
+printf "%s" "Generating strict schema ... "
+python3 "${STRICT_GENERATOR}" "${ORIG_SCHEMA}" > "${STRICT_SCHEMA}"
+printf "%s\n" "done"
+
+SCHEMA=${STRICT_SCHEMA}
+test_all
+
+exit ${FAIL}
