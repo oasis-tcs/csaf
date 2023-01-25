cf3be4806626d9246d2aca58e991442de345942f
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2023-01-25 01:24:45 +0100

    Testcases
    - addresses parts of oasis-tcs/csaf#599
    - extend testscript to verify files exist in testfiles.json and in git
    - correct mistakes for 6.2.8 and 6.2.9

--- csaf_2.0/test/validator/check_testcases.sh
+++ csaf_2.0/test/validator/check_testcases.sh
@@ -2,9 +2,12 @@
 
 SCHEMA=csaf_2.0/test/validator/testcases_json_schema.json 
 VALIDATOR=csaf_2.0/test/validator.py
-TESTPATH=csaf_2.0/test/validator/data/testcases.json
+TESTPATH=csaf_2.0/test/validator/data/
+TESTFILE=testcases.json
 
 FAIL=0
+DIRFILES=""
+TESTFILES=""
 
 # go to root of git repository
 cd `dirname $0`/../../..
@@ -20,6 +23,33 @@ validate() {
 
 }
 
-validate ${TESTPATH}
+collect_dir_files() {
+  printf "%s\n" "Collect list of files in $1 ... "
+  cd $1
+  DIRFILES=$(ls -1 */*.json | sort)
+}
+
+collect_test_files() {
+  printf "%s\n" "Collect list of files in $1 ..."
+  NEGATIVE=$(jq '.tests[].failures[].name' $1 -r)
+  POSITIVE=$(jq '.tests[].valid[]?.name' $1 -r)
+  TESTFILES=$(echo -e "${NEGATIVE}\n${POSITIVE}" | sort)
+}
+
+check_files_exist() {
+  printf "%s" "Testing files in ${TESTPATH} and ${TESTFILE} are the same... "
+    if diff -b <(echo "$1") <(echo "$2"); then
+    printf "%s\n" SUCCESS
+  else
+    printf "%s\n" FAILED
+    FAIL=1
+  fi
+}
+
+#validate "${TESTPATH}${TESTFILE}"
+collect_dir_files ${TESTPATH}
+collect_test_files "${TESTFILE}"
+check_files_exist "${DIRFILES}" "${TESTFILES}"
+
 
 exit ${FAIL}
