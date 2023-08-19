88acc8bbeb1bac0d37bc21c4c638a070f4327d61
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2023-08-19 02:42:31 +0200

    Resolving refs in jsonschema
    - addresses parts of oasis-tcs/csaf#643
    - link CVSS schemas for test of examples

--- csaf_2.0/test/csaf_schema/run_tests.sh
+++ csaf_2.0/test/csaf_schema/run_tests.sh
@@ -2,6 +2,9 @@
 
 ORIG_SCHEMA=csaf_2.0/json_schema/csaf_json_schema.json 
 STRICT_SCHEMA=csaf_strict_schema.json
+CVSS_20_STRICT_SCHEMA=csaf_2.0/referenced_schema/first/cvss-v2.0_strict.json
+CVSS_30_STRICT_SCHEMA=csaf_2.0/referenced_schema/first/cvss-v3.0_strict.json
+CVSS_31_STRICT_SCHEMA=csaf_2.0/referenced_schema/first/cvss-v3.1_strict.json
 VALIDATOR=csaf_2.0/test/validator.py
 STRICT_GENERATOR=csaf_2.0/test/generate_strict_schema.py
 TESTPATH=csaf_2.0/examples/csaf/$1/*.json
@@ -13,7 +16,7 @@ cd `dirname $0`/../../..
 
 validate() {
   printf "%s" "Testing file $1 against schema ${SCHEMA} ... "
-  if python3 ${VALIDATOR} ${SCHEMA} $1; then
+  if python3 ${VALIDATOR} ${SCHEMA} $1 ${CVSS_20_STRICT_SCHEMA} ${CVSS_30_STRICT_SCHEMA} ${CVSS_31_STRICT_SCHEMA}; then
     printf "%s\n" SUCCESS
   else
     printf "%s\n" FAILED
