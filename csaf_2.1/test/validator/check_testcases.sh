a00df803be649827c14b956c8a7a5b9a0c4ad32a
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2025-04-29 23:18:37 +0200

    Format validation
    - addresses parts of oasis-tcs/csaf#813
    - fix missing metaschema import

--- csaf_2.1/test/validator/check_testcases.sh
+++ csaf_2.1/test/validator/check_testcases.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCHEMA=csaf_2.1/test/validator/testcases_json_schema.json
+META_SCHEMA=csaf_2.1/json_schema/meta_json_schema.json
 VALIDATOR=csaf_2.1/test/validator.py
 TESTPATH=csaf_2.1/test/validator/data/
 TESTFILE=testcases.json
@@ -14,7 +15,7 @@ cd `dirname $0`/../../..
 
 validate() {
   printf "%s" "Testing file $1 against schema ${SCHEMA} ... "
-  if python3 $VALIDATOR $SCHEMA $1; then
+  if python3 $VALIDATOR $SCHEMA $1 ${META_SCHEMA}; then
     printf "%s\n" SUCCESS
   else
     printf "%s\n" FAILED
