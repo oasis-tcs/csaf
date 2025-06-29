463546fee4b732375fd273a20a477e60c91dd0b5
Stefan Hagen <stefan@hagen.link>
2025-06-29 21:23:47 +0200

    Added the title of the spec as value of the title tag in HTML
    - Closes #1072
    
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- csaf_2.1/prose/edit/bin/toccata.py
+++ csaf_2.1/prose/edit/bin/toccata.py
@@ -25,6 +25,10 @@ LOGO_AT = pathlib.Path('..') / 'media' / 'logo-data-url.txt'
 BASE_CSS_AT = pathlib.Path('..') / 'share' / 'style' / 'base.css'
 SKIN_CSS_AT = pathlib.Path('..') / 'share' / 'style' / 'skin.css'
 
+IN_TITLE_TRIGGER_IS = '<title>'
+TITLE_INNER_HTML_MARKER_IS = 'tmp'
+PATCH_TITLE_INNER_HTML = 'Common Security Advisory Framework Version 2.1'
+
 # Type declarations:
 META_TOC_TYPE = dict[str, dict[str, Union[bool, str, list[dict[str, str]]]]]
 SEC_LVL_CNT_TYPE = tuple[int, int, int, int, int, int]
@@ -109,6 +113,8 @@ def main(argv: list[str]) -> int:
     outgoing = []
     in_toc = False
     for line in incoming:
+        if TITLE_INNER_HTML_MARKER_IS in line:
+            line = line.replace(TITLE_INNER_HTML_MARKER_IS, PATCH_TITLE_INNER_HTML)
         if in_toc:
             if line.startswith(INTRO_STARTSWITH_TRIGGER):
                 in_toc = False
