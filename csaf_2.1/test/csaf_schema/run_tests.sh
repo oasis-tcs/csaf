3767b7c1a7c65eedb4a9ce6af80d478edea0fe82
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2026-02-21 23:09:55 +0100

    Extensions
    - addresses parts of oasis-tcs/csaf#1077
    - add missing reference to CSAF Extension Content Schema to test script
    - still keep invalid examples of 6.1.60 to check that test works

--- csaf_2.1/test/csaf_schema/run_tests.sh
+++ csaf_2.1/test/csaf_schema/run_tests.sh
@@ -7,6 +7,8 @@ CVSS_20_STRICT_SCHEMA=csaf_2.1/referenced_schema/first/cvss-v2.0_strict.json
 CVSS_30_STRICT_SCHEMA=csaf_2.1/referenced_schema/first/cvss-v3.0_strict.json
 CVSS_31_STRICT_SCHEMA=csaf_2.1/referenced_schema/first/cvss-v3.1_strict.json
 CVSS_40_STRICT_SCHEMA=csaf_2.1/referenced_schema/first/cvss-v4.0.1_strict.json
+EXTENSION_CONTENT_SCHEMA=csaf_2.1/json_schema/extension-content.json
+EXTENSION_META_SCHEMA=csaf_2.1/json_schema/extension-metaschema.json
 META_SCHEMA=csaf_2.1/json_schema/meta.json
 SSVC_200_SL_SCHEMA=csaf_2.1/referenced_schema/certcc/SelectionList_2_0_0.schema.json
 VALIDATOR=csaf_2.1/test/validator.py
@@ -20,7 +22,7 @@ cd `dirname $0`/../../..
 
 validate() {
   printf "%s" "Testing file $1 against schema ${SCHEMA} ... "
-  if python3 ${VALIDATOR} ${SCHEMA} $1 ${CVSS_20_STRICT_SCHEMA} ${CVSS_30_STRICT_SCHEMA} ${CVSS_31_STRICT_SCHEMA} ${CVSS_40_STRICT_SCHEMA} ${META_SCHEMA} ${SSVC_200_SL_SCHEMA}; then
+  if python3 ${VALIDATOR} ${SCHEMA} $1 ${CVSS_20_STRICT_SCHEMA} ${CVSS_30_STRICT_SCHEMA} ${CVSS_31_STRICT_SCHEMA} ${CVSS_40_STRICT_SCHEMA} ${META_SCHEMA} ${SSVC_200_SL_SCHEMA} ${EXTENSION_CONTENT_SCHEMA} ${EXTENSION_META_SCHEMA}; then
     printf "%s\n" SUCCESS
   else
     printf "%s\n" FAILED
